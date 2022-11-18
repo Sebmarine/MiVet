15b6f068f0087411d28726327a83a94243600906
Sebmarine <110854189+Sebmarine@users.noreply.github.com>
2022-11-18 15:40:12 -0800

    Add files via upload

---
+++ .Net/ExternalLinksAddRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.ExternalLinks
+{
+    public class ExternalLinksAddRequest
+    {
+        [Required]
+        [Range(1, Int32.MaxValue)]
+        public int UrlTypeId { get; set; }
+
+        [Required]
+        [DataType(DataType.Url)]
+        [MinLength(2), MaxLength(255)]
+        public string Url { get; set; } 
+
+        [Required]
+        [Range(1, Int32.MaxValue)]
+        public int EntityId { get; set; }
+
+        [Required]
+        [Range(1, Int32.MaxValue)]
+        public int EntityTypeId { get; set; } 
+    }
+}
